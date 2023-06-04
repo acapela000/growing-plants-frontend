87fc0b7964bc0a6c66f69e9fd6736ad21c646b75
jorgechato <hack@jorgechato.com>
2023-06-04 15:00:49 +0900

    UPDATE types & user card

--- src/components/Trees.tsx
+++ src/components/Trees.tsx
@@ -3,33 +3,47 @@ import { RigidBody } from "@react-three/rapier";
 import { TreeOne } from "./Models/Trees/Tree-1";
 import { TreeTwo } from "./Models/Trees/Tree-2";
 import { TreeThree } from "./Models/Trees/Tree-3";
+import { TreeFour } from "./Models/Trees/Tree-4";
 
 
 type TreesProps = {
     seeds: number;
 }
 
-type Tree = "TreeOne" | "TreeTwo" | "TreeThree";
+type Tree = "TreeOne" | "TreeTwo" | "TreeThree" | "TreeFour";
 
 
 export function Trees(props: TreesProps) {
     let remainderSeeds: number = props.seeds;
 
+    const generateRandomNumber = () => {
+        const randomNum = Math.random();
+        if (randomNum < 0.95) {
+            return Math.floor(randomNum * 3);
+        } else {
+            return 3;
+        }
+    }
+
     // TreeOne is 1500 seeds, TreeTwo is 5000 seeds, TreeThree is 3700 seeds
     const randomTrees = () => {
         const trees: Tree[] = [];
         while (remainderSeeds > 0) {
-            const randomNum = Math.floor(Math.random() * 3);
+            const randomNum = generateRandomNumber();
             let tree: Tree;
+
             if (randomNum === 0 && remainderSeeds >= 1500) {
                 tree = "TreeOne";
                 remainderSeeds -= 1500;
             } else if (randomNum === 1 && remainderSeeds >= 5000) {
                 tree = "TreeTwo";
                 remainderSeeds -= 5000;
-            } else if (remainderSeeds >= 3700) {
+            } else if (randomNum === 2 && remainderSeeds >= 3700) {
                 tree = "TreeThree";
                 remainderSeeds -= 3700;
+            } else if (remainderSeeds >= 10000) {
+                tree = "TreeFour";
+                remainderSeeds -= 5700;
             } else {
                 break;
             }
@@ -39,25 +53,25 @@ export function Trees(props: TreesProps) {
     }
 
     return (
-        <>
-            <RigidBody colliders={false}>
-                <group>
-                    {randomTrees().map((tree, index) => {
-                        const left: number = (Math.random() * 13.9) - 6.8;
-                        const right: number = (Math.random() * 13.9) - 6.8;
-                        const rotation: number = Math.random() * Math.PI * 2;
+        <RigidBody colliders={false}>
+            <group>
+                {randomTrees().map((tree, index) => {
+                    const left: number = (Math.random() * 13.9) - 6.8;
+                    const right: number = (Math.random() * 13.9) - 6.8;
+                    const rotation: number = Math.random() * Math.PI * 2;
 
-                        switch (tree) {
-                            case "TreeOne":
-                                return <TreeOne key={index} scale={[2, 2, 2]} position={[left, -.5, right]} rotation-y={rotation} />
-                            case "TreeTwo":
-                                return <TreeTwo key={index} scale={[1, 1, 1]} position={[left, .4, right]} rotation-y={rotation} />
-                            case "TreeThree":
-                                return <TreeThree key={index} scale={[1, 1, 1]} position={[left, .5, right]} rotation-y={rotation} />
-                        }
-                    })}
-                </group>
-            </RigidBody>
-        </>
+                    switch (tree) {
+                        case "TreeOne":
+                            return <TreeOne key={index} scale={[2, 2, 2]} position={[left, -.5, right]} rotation-y={rotation} />
+                        case "TreeTwo":
+                            return <TreeTwo key={index} scale={[1, 1, 1]} position={[left, .4, right]} rotation-y={rotation} />
+                        case "TreeThree":
+                            return <TreeThree key={index} scale={[1, 1, 1]} position={[left, .5, right]} rotation-y={rotation} />
+                        case "TreeFour":
+                            return <TreeFour key={index} scale={[.03, .03, .03]} position={[left, -1.5, right]} rotation-y={rotation} />
+                    }
+                })}
+            </group>
+        </RigidBody>
     )
 };
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,5 +1,3 @@
-
-
 type UserCardProps = {
     level: string;
     id: string;
@@ -8,18 +6,15 @@ type UserCardProps = {
 
 export function UserCard(props: UserCardProps) {
     return (
-        <>
-            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow">
-                <div className="flex flex-col items-center pb-10 pt-10">
-                    <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src="https://avatars.githubusercontent.com/u/106527693?v=4" />
-                    <h5 className="mb-1 text-xl font-medium text-gray-900">{props.level}</h5>
-                    <span className="text-xs text-gray-500">{props.id}</span>
-                    <h5 className="mb-1 text-xl font-medium text-gray-900">
-                        <img src="/seed.png" className="w-6 inline" /> {props.seeds}
-                    </h5>
-                </div>
+        <div className="max-w-[250px] bg-white border border-gray-200 rounded-lg shadow relative">
+            <div className="flex flex-col items-center pb-10 pt-10">
+                <img className="w-24 h-24 border-4 border-white mb-3 rounded-full shadow-lg absolute -top-12 left-1/2 transform -translate-x-1/2" src="https://avatars.githubusercontent.com/u/106527693?v=4" />
+                <h5 className="mb-1 mt-4 text-xl font-medium text-gray-900">{props.level}</h5>
+                <span className="text-xs text-gray-500">{props.id}</span>
+                <h5 className="mb-1 text-xl font-medium text-gray-900">
+                    <img src="/seed.png" className="w-6 inline" /> {props.seeds}
+                </h5>
             </div>
-
-        </>
+        </div>
     )
 }
