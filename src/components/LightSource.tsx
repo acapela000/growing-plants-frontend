9d573282e0ac135797a4ebf17ddc91df2a87e2e1
thao <mrtrados@gmail.com>
2023-11-05 21:56:21 +0900

    UPDATE fix LightSource

--- src/components/LightSource.tsx
+++ src/components/LightSource.tsx
@@ -4,10 +4,10 @@ import React, { useRef } from "react"
 
 export default function LightSource() {
     const sunTexture = useTexture("/lensflare0.png")
-    const sunLight = useRef("");
+    const sunLight = useRef<any>(0);
 
     useFrame(({ clock }) => {
-        sunLight.current += 0.003
+        sunLight.current.rotation.z += 0.003
     })
     return (
         <>
@@ -16,7 +16,7 @@ export default function LightSource() {
                     <meshStandardMaterial map={sunTexture} alphaToCoverage emissive="#fff29c" color="yellow" transparent fog={false} />
                 </Circle>
             </Billboard>
-            <directionalLight castShadow color="orange" position={[7, 25, -20]} intensity={3.2} shadowMapHeight={256} shadowMapWidth={256} shadow-bias={-0.006} shadow-normalBias={0.01}>
+            <directionalLight castShadow color="orange" position={[7, 25, -20]} intensity={3.2} shadow-mapSize-height={256} shadow-mapSize-width={256} shadow-bias={-0.006} shadow-normalBias={0.01}>
                 <orthographicCamera attach="shadow-camera" args={[-25, 25, -25, 25, 0.1, 50]} />
             </directionalLight>
             <ambientLight color={"lightblue"} intensity={0.9} />
